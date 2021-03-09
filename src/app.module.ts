3e87fba072ef60032063ea07482a15a59c32723e
azukiazusa1 <cycleippei@gmail.com>
2021-03-09 23:32:15 +0900

    graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    BooksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
